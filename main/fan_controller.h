dc2810075629875020bcffa4341a8ca5dd9ee669
Nathan Begin <nathanbegin@hotmail.com>
2026-09-15 11:29:20 -0400

    Simplifie le contrôleur de ventilation à trois relais

--- main/fan_controller.h
+++ main/fan_controller.h
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-/* Logical coil states, independent of the electrical HIGH/LOW polarity. */
+/* États logiques de ventilation. */
 typedef enum { FAN_OFF = 0, FAN_LOW = 1, FAN_HIGH = 2 } fan_mode_t;
 typedef enum { FAN_SWITCH_LOW, FAN_SWITCH_HIGH, FAN_SWITCH_EXCHANGE } fan_switch_t;
 typedef struct {
@@ -23,7 +23,14 @@ typedef struct {
     bool healthy;
 } fan_controller_t;
 
-/* Call from one owner task. Relay indexes 0..3 correspond to K1..K4. */
+/*
+ * Indexes relais :
+ *   0 = K1 Fan Low  (branche 21 kΩ)
+ *   1 = K2 Fan High (branche 4 kΩ)
+ *   2 = K3 échange extérieur (liaison directe J13-J14)
+ *
+ * Invariant : exchange=true n'est valide qu'en FAN_HIGH.
+ */
 bool fan_controller_init(fan_controller_t *fan, fan_io_t io);
 bool fan_controller_apply(fan_controller_t *fan, fan_state_t target);
 bool fan_controller_switch(fan_controller_t *fan, fan_switch_t control, bool on);
